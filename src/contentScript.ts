7db3d1dd6815e4ae2448884069bc27b49e56450a
zshnb <a857681664@gmail.com>
2023-11-21 18:32:42 +0800

    fix: overlay's position

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -1,12 +1,39 @@
 import mountFloatingDiv from "./mountFloatingDiv";
+import debounce from "debounce";
 
-document.addEventListener('dblclick', () => {
-  console.log('clicked')
-  const selectedText = window.getSelection()?.toString().trim();
-  if (selectedText) {
-    // 这里调用React组件来显示悬浮框
-    mountFloatingDiv(selectedText);
+const handleSelectionChange = () => {
+  const selection = window.getSelection();
+  if (selection && selection.rangeCount > 0 && selection.toString().trim() !== '') {
+    const selectedText = selection.toString().trim()
+    const range = selection.getRangeAt(0).getBoundingClientRect();
+    const marker = document.createElement('div');
+
+    console.log(range)
+    // 设置小方块的样式
+    marker.style.position = 'absolute';
+    marker.style.left = `${range.right}px`;
+    marker.style.top = `${range.bottom}px`;
+    marker.style.width = '20px';
+    marker.style.height = '20px';
+    const imageUrl = chrome.runtime.getURL('assets/logo.png');
+    marker.style.backgroundImage = `url("${imageUrl}")`;
+    marker.style.backgroundSize = 'cover'
+
+    document.body.appendChild(marker);
+
+    // 小方块的点击事件
+    marker.addEventListener('click', () => {
+      mountFloatingDiv(selectedText, {
+        position: {
+          left: range?.left || 0,
+          bottom: range?.bottom || 0
+        }
+      });
+      document.body.removeChild(marker)
+    });
   }
-});
+}
+
+document.addEventListener('selectionchange', debounce(handleSelectionChange, 300));
 
 export {}
--- src/mountFloatingDiv.tsx
+++ src/mountFloatingDiv.tsx
@@ -1,8 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import useTranslate from "./useTranslate";
+import styled from "styled-components";
 
-const FloatingDiv: React.FC<{ text: string; onClose: () => void }> = ({ text, onClose }) => {
+export type FloatingDivProps = {
+  text: string
+  onClose: () => void
+  position: {
+    left: number
+    bottom: number
+  }
+}
+
+const TranslationText = styled.p`
+  margin: 0;
+`
+const FloatingDiv: React.FC<FloatingDivProps> = ({ text, onClose, position }) => {
   const { translate, translation, loading } = useTranslate(); // 使用hook
 
   useEffect(() => {
@@ -21,18 +34,28 @@ const FloatingDiv: React.FC<{ text: string; onClose: () => void }> = ({ text, on
   }, [text, onClose]);
 
   return (
-    <div style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)', backgroundColor: 'white', padding: '10px', borderRadius: '5px', boxShadow: '0 2px 4px rgba(0,0,0,0.2)' }}>
-      <p>{loading ? '翻译中' : translation}</p>
+    <div style={{
+      position: 'absolute',
+      left: position.left,
+      top: position.bottom,
+      backgroundColor: 'white',
+      padding: '10px',
+      borderRadius: '5px',
+      boxShadow: '0 2px 4px rgba(0,0,0,0.2)'
+    }}>
+      <TranslationText>{loading ? '翻译中' : translation}</TranslationText>
     </div>
   );
 };
 
-const mountFloatingDiv = (text: string) => {
+const mountFloatingDiv = (text: string, position: Pick<FloatingDivProps, 'position'>) => {
   const floatingDivContainer = document.createElement('div');
   document.body.appendChild(floatingDivContainer);
 
   ReactDOM.render(
-    <FloatingDiv text={text} onClose={() => document.body.removeChild(floatingDivContainer)} />,
+    <FloatingDiv text={text} onClose={() => {
+      document.body.removeChild(floatingDivContainer)
+    }} position={position.position}/>,
     floatingDivContainer
   );
 };
